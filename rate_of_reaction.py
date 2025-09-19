cbd1484eb797b24eafe7d60cccf99f39a5f5b1c2
Yuvraj. <itsyuvii42@gmail.com>
2025-09-19 20:03:24 +0530

    Update rate_of_reaction.py

--- rate_of_reaction.py
+++ rate_of_reaction.py
@@ -1,11 +1,7 @@
 import math
-
-
 t = 0
-
 a = 125
 b = 62.5
-
 for t in range (5,35,5):
     x = float(input(f"enter 'x' for {t} minutes : "))
     d = a-x
@@ -14,8 +10,6 @@ for t in range (5,35,5):
     f = a*D
     l = math.log10(g/f)
     k = (2.303/(t*b)) * l
-    
-
     print(f"(a-x) for {t} minutes is :{d}")
     print(f"(b-x) for {t} minutes is :{D}")
     print(f"log b(a-x)/a(b-x) for {t} minutes is :{l}")
